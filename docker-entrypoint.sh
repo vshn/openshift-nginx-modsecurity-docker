d4600fdbb2e2c042f108dbe9da9be45561557e8a
Joao Pinto <joao.pinto@vshn.ch>
2020-01-30 17:30:38 +0100

    Add an healthcheck url

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,12 @@ cat | envsubst '${ROUTER_PROXY_IN_HTTP_PORT} ${ROUTER_PROXY_OUT_HTTP_PORT}' \
 > ${NGINX_CONFIGURATION_PATH}/middle_proxy.conf  << _EOF_
 server {
     listen ${ROUTER_PROXY_IN_HTTP_PORT};
-            location / {
-            proxy_pass http://127.0.0.1:${ROUTER_PROXY_OUT_HTTP_PORT}/;
-        }
+    location /nginx-health {
+        return 200 "healthy\n";
+    }
+        location / {
+        proxy_pass http://127.0.0.1:${ROUTER_PROXY_OUT_HTTP_PORT}/;
+    }
 }
 _EOF_
 }
