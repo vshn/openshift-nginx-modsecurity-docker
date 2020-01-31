cbb722e1443ded977ce79a668d37dd8b409e778d
Joao Pinto <joao.pinto@vshn.ch>
2020-01-31 14:38:55 +0100

    Add support for ROUTER_PROXY_MODSECURITY_RULES_FILE

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 function middle_proxy_setup {
-cat | envsubst '${ROUTER_PROXY_IN_HTTP_PORT} ${ROUTER_PROXY_OUT_HTTP_PORT}' \
+SUBSTVARS='${ROUTER_PROXY_IN_HTTP_PORT} ${ROUTER_PROXY_OUT_HTTP_PORT} ${ROUTER_PROXY_MODSECURITY_RULES_FILE}'
+cat | envsubst "$SUBSTVARS" \
 > ${NGINX_CONFIGURATION_PATH}/middle_proxy.conf  << _EOF_
 server {
     listen ${ROUTER_PROXY_IN_HTTP_PORT};
@@ -12,6 +13,8 @@ server {
         proxy_set_header Host \$host;
         proxy_pass http://127.0.0.1:${ROUTER_PROXY_OUT_HTTP_PORT}/;
     }
+    modsecurity on;
+    modsecurity_rules_file ${ROUTER_PROXY_MODSECURITY_RULES_FILE};
 }
 _EOF_
 }
